8041e1534e349f6697b6a04597925b2c8bcb91db
Adlyn Khairudin <adlynkhairudin@gmail.com>
2025-07-04 09:15:54 +0800

    Fix LinkedIn button linkon NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/adlyn-khairudin-560046219/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Icon" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
